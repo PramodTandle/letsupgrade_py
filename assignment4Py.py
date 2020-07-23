d871f69eb57563a796eb855bc4fb9fb9f60815bc
PramodTandle <66490519+PramodTandle@users.noreply.github.com>
2020-07-24 00:23:11 +0530

    Add files via upload

---
+++ assignment4Py.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+p="what we think we become; we are python programmer"
+p.index("we")
+
+
+# In[11]:
+
+
+p="What we think we become; we are python programmer"
+p.rindex("we")
+
+
+# In[13]:
+
+
+p="What we think we become; we are python programmer"
+p.rfind("python")
+
+
+# In[17]:
+
+
+p="What we think we become; we are python programmer"
+p.find(";")
+
+
+# In[24]:
+
+
+x="pramod tandle"
+x.islower()
+
+
+# In[19]:
+
+
+x="Pramod tandle"
+x.isupper()
+
+
+# In[ ]:
+
+
+x="Pramod Tandle"
+x.istitle()
+
+
+# In[ ]:
+
+
+x="pramod tandle"
+x.islower()
+
+
+# In[ ]:
+
+
+val=input("eneter string:")
+val.islower()
+
+
+# In[ ]:
+
+
+
+
